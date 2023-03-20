c70f61e7a745bc1f835196c9fe68dcb43fcb1055
Devin Minnihan <devinminnihan@gmail.com>
2023-03-20 09:15:18 -0500

    photo fix

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -3,25 +3,25 @@ import Image from "next/image";
 
 const Carousel = ({ images }: { images: string[] }) => {
   return (
-    <div className="mx-auto max-w-md overflow-scroll rounded-md border-2 border-transparent shadow-md">
-      <div className="carousel carousel-center flex items-center rounded-md">
+    <div className="mx-auto max-w-md overflow-scroll rounded-md border-2 border-transparent shadow-sm">
+      <div className="carousel flex items-center rounded-md pt-4">
         {images.map((image) => (
           <div
             key={image}
             id={image.toString()}
-            className="carousel-item flex w-full items-center justify-center"
+            className="carousel-item flex w-full items-center justify-center relative h-[50vh]"
           >
             <Image
               src={image}
-              height={200}
-              width={400}
+              fill
               alt="image"
               key={image}
+              className="object-contain py-4"
             />
           </div>
         ))}
       </div>
-      <div className="flex w-full justify-center gap-2 py-2 font-extralight text-neutral-200">
+      <div className="flex w-full justify-center gap-2 pb-2 font-extralight text-neutral-200">
         {images.map((image, i) => (
           <a
             key={image}
