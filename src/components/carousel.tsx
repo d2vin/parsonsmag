cbf0c5b8d5371b0ce4810e6b3fa49e571456b298
Devin Minnihan <devinminnihan@gmail.com>
2023-04-04 02:28:00 -0400

    add changes

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 const Carousel = ({ images }: { images: string[] }) => {
   return (
-    <div className="mx-auto max-w-md overflow-scroll rounded-md border-2 border-transparent shadow-sm">
+    <div className="no-scrollbar mx-auto max-w-md overflow-scroll rounded-md border-2 border-transparent shadow-sm">
       <div className="carousel flex items-center rounded-md pt-4">
         {images.map((image) => (
           <div
             key={image}
             id={image.toString()}
-            className="carousel-item flex w-full items-center justify-center relative h-[50vh]"
+            className="carousel-item relative flex h-[50vh] w-full items-center justify-center"
           >
             <Image
               src={image}
@@ -23,11 +23,7 @@ const Carousel = ({ images }: { images: string[] }) => {
       </div>
       <div className="flex w-full justify-center gap-2 pb-2 font-extralight text-neutral-200">
         {images.map((image, i) => (
-          <a
-            key={image}
-            href={`#${image}`}
-            className="btn-xs btn"
-          >
+          <a key={image} href={`#${image}`} className="btn-xs btn">
             {i}
           </a>
         ))}
